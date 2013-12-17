87c253f8c0026e5b298fb05a4120ea2e5dce2751
Brian Folts <brian.folts@gmail.com>
2013-12-17 13:03:09 -0700

    Update Addition.java
    javadoc added.

--- src/translator/Addition.java
+++ src/translator/Addition.java
@@ -1,7 +1,17 @@
 package translator;
 
+/**
+  * This class represents an Addition within the Java language.
+  * This is represented in the Java source code by the '+' character.
+  *
+  * @author  Brian Folts
+  * @since   1.0
+  */
 public class Addition {
 
+	/**
+	 * Default addition constructor.
+	 */
 	public Addition() {
 	
 	}
